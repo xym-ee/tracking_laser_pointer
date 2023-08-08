d1b4bd4159a62fff149d0c3406e496b5964f60a5
xym-win <565000561@qq.com>
2023-08-08 13:24:45 +0800

    调整AOI，参数优化

--- main.py
+++ main.py
@@ -1,60 +1,48 @@
-# Hello World Example
-#
-# Welcome to the OpenMV IDE! Click on the green run arrow button below to run the script!
+import sensor, image, time, pyb
 
-import sensor, image, time
-import pyb
-
-sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
-sensor.skip_frames(time = 2000)     # Wait for settings take effect.
-
-
-sensor.set_auto_gain(False)         # 关闭自动自动增益。默认开启的，在颜色识别中，一定要关闭白平衡。
-sensor.set_auto_whitebal(False)     #关闭白平衡。白平衡是默认开启的，在颜色识别中，一定要关闭白平衡。
-
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing((180, 180))
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
 
 clock = time.clock()                # Create a clock object to track the FPS.
 
+led     = pyb.LED(3)
+uart    = pyb.UART(3,115200)
 
-led = pyb.LED(3)
-
-red_point = (24, 100, 14, 127, -128, 127)
-
-i = 1
+# red_point = (24, 100, 14, 127, -128, 127)
+red_point = (30, 100, 20, 127, -128, 127)
 
 while(True):
     clock.tick()                    # Update the FPS clock.
     img = sensor.snapshot()         # Take a picture and return the image.
-    
-    
+
     blobs = img.find_blobs([red_point])
-    
+
     if blobs:
-    
-        led.on()
-    
         for b in blobs:
 
-            img.draw_rectangle(b[0:4]) # Draw a rect around the blob.
+            # 坐标旋转
+            x = 179 - b.cy()
+            y = b.cx()
 
-            img.draw_cross(b[5], b[6]) # (x, y) 画十字形标记
+            # 串口输出
+            uart_output = "%c%c%c%c%c" % (0x55, 0x55, x, y, 0xAA)
+            uart.write(uart_output)
 
-            print(b[5],b[6])
-            
-    
-    # print(clock.fps()) # 注意: 你的OpenMV连到电脑后帧率大概为原来的一半
-    
+            # -----------------------------------------------------------------调试用
+            led.on()
+            img.draw_rectangle(b[0:4])          # 调试用 Draw a rect around the blob.
+            img.draw_cross(b[5], b[6])          # 调试用 (x, y) 画十字形标记
+
+            output_str = "[%d,%d]" % (x, y)     # 调试用输出坐标
+            print(output_str)
+            print(uart_output)
+            # -----------------------------------------------------------------调试用
     else:
         led.off()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    #print(clock.fps()) # 注意: 你的OpenMV连到电脑后帧率大概为原来的一半
